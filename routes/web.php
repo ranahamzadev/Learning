38b1674dd61c547cbb2d1276c5d66b680db8b8eb
Rana Hamza <ranahamza.dev@gmail.com>
2025-09-08 00:58:20 +0500

    routes added

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,20 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+// Route::get('/customer', [CustomerController::class, 'index'])->name('customer.index');
+// Route::get('/customer/create', [CustomerController::class, 'create'])->name('customer.create');
+// Route::post('/customer', [CustomerController::class, 'store'])->name('customer.store');
+// Route::get('/customer/{customer}/edit', [CustomerController::class, 'edit'])->name('customer.edit');
+// Route::put('/customer/{customer}', [CustomerController::class, 'update'])->name('customer.update');
+// Route::get('/customer/{customer}', [CustomerController::class, 'show'])->name('customer.show');
+// Route::delete('/customer/{customer}', [CustomerController::class, 'destroy'])->name('customer.destroy');
+
+Route::resource('customer', CustomerController::class);
+
+// php artisan route:list
